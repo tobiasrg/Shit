dddc063554bdad473dcb844efcb64e621f1cc681
tobiasrg <tobrydberg@gmail.com>
2016-07-31 16:20:12 +0200

    Laptop

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,8 +1,13 @@
-/**
- * Created by Tobias on 2016-07-31.
- */
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        String name;
+        String message;
+
+        name = JOptionPane.showInputDialog("Vad heter du?");
+        message = "håll käften " + name;
+        JOptionPane.showMessageDialog(null, message);
 
         System.out.println("Hello World från laptop");
     }
