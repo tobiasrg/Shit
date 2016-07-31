8d61f3c24fd0aede874ccbccb6fd4fd860d8ce4d
tobiasrg <tobrydberg@gmail.com>
2016-07-31 15:27:43 +0200

    Laptop

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -2,7 +2,8 @@
  * Created by Tobias on 2016-07-31.
  */
 public class Main {
-    public static void main(String [] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+
+        System.out.println("Hello World från laptop");
     }
 }
